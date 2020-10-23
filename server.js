3a08283f10c7807213cb22b87b21ff183fb20b48
Prit Shah <prit.shah@itential.com>
2020-10-23 10:44:29 -0400

    update profile

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// GET call to localhost:5000/ returns API running
 app.get('/', (req, res) => res.send('API running'));
 
-// Define Routes
+// Define API Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+// Process.env.PORT looks for an environment variable called PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
