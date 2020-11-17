5e67236f5e8248e6f4362dd37b3285da13be22e2
Prit Shah <prit.shah@itential.com>
2020-11-16 21:33:35 -0500

    publishing updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { connect } = require('mongoose');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,14 +11,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// GET call to localhost:5000/ returns API running
-app.get('/', (req, res) => res.send('API running'));
-
 // Define API Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Process.env.PORT looks for an environment variable called PORT
 const PORT = process.env.PORT || 5000;
